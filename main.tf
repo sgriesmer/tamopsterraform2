2fdae54a92af32e7a7e434de6da765102421081c
sgriesmer <sgriesmer@comcast.net>
2021-08-02 21:02:44 -0400

    Update main.tf
    Changing test terraform to create a different resource group

--- main.tf
+++ main.tf
@@ -13,17 +13,13 @@ terraform {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "example" {
-  name     = "tamops-tf"
-  location = "eastus"
-}
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "myResourceGroup"
+    location = "eastus"
 
-resource "azurerm_storage_account" "example" {
-  name                     = "tamopssatf1278"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+    tags = {
+        environment = "Terraform Demo"
+    }
 }
 
 
