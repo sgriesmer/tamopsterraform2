c7b85e68709afa303325cf21a7214b73560dc01e
sgriesmer <sgriesmer@comcast.net>
2021-08-02 21:15:43 -0400

    Update main.tf
    Add NSG to allow ssh traffic

--- main.tf
+++ main.tf
@@ -33,6 +33,27 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
     }
 }
 
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
 resource "azurerm_subnet" "myterraformsubnet" {
     name                 = "mySubnet"
     resource_group_name  = azurerm_resource_group.myterraformgroup.name
