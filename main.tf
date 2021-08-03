7e7228212c7c017f01ca759dddd384b186d18a2c
sgriesmer <sgriesmer@comcast.net>
2021-08-02 21:08:36 -0400

    Update main.tf
    Adding subnet

--- main.tf
+++ main.tf
@@ -32,3 +32,10 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
         environment = "Terraform Demo"
     }
 }
+
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = azurerm_resource_group.myterraformgroup.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefixes       = ["10.0.2.0/24"]
+}
