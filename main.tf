f7df767e829058a56ba6627423869a8223811281
sgriesmer <sgriesmer@comcast.net>
2021-08-02 21:29:15 -0400

    Update main.tf
    Add VM with username/pw auth

--- main.tf
+++ main.tf
@@ -33,6 +33,39 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
     }
 }
 
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+    name                  = "myVM"
+    location              = "eastus"
+    resource_group_name   = azurerm_resource_group.myterraformgroup.name
+    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    size                  = "Standard_DS1_v2"
+
+    os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "myvm"
+    admin_username = "azureuser"
+    admin_password = "Try1t^now"
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
