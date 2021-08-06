5e40b4b44604db110dff0d6d4d5d2afcf5ab9e64
sgriesmer <sgriesmer@comcast.net>
2021-08-06 11:23:10 -0400

    Update main.tf
    Updated main.tf to one in exercise; it will break the build

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,155 +1,101 @@
 provider "azurerm" {
-    features {}
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  features {}
 }
 
 terraform {
-        backend "azurerm" {
-            storage_account_name = "tamopstf2121"
-            container_name       = "tfstatedevops"
-            key                  = "terraform.tfstate"
-            access_key           = "eLOs0wXAtFmH5kwOAS0SJiUsWVLt9cleqDoDYiAcYa2GWZPFA1v8yQj335Vp7KGAgL6e+LWrUi27ybwMcPwsNw=="
-            }
-        }
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
-    location = "eastus"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+  backend "azurerm" {
+    storage_account_name = "tstate21911"
+    container_name       = "tstate"
+    key                  = "terraform.tfstate"
+    access_key           = "yGfoFhhSJwpUS5RHmuE6vru7FT51Py64oPprrzZcs8xbrfVUs4yzizd88DKLyEkHaOCv1q0GSibj1MvoWNDrnQ=="
+  }
 }
 
-resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+module "resource_group" {
+  source               = "./modules/resource_group"
+  resource_group       = "${var.resource_group}"
+  location             = "${var.location}"
 }
 
-resource "azurerm_linux_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
-    location              = "eastus"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-    size                  = "Standard_DS1_v2"
+module "network" {
+  source               = "./modules/network"
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
+  resource_type        = "NET"
+  resource_group       = "${module.resource_group.resource_group_name}"
+  address_prefix_test  = "${var.address_prefix_test}"
+}
 
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
-    }
+module "nsg-test" {
+  source           = "./modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "NSG"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
+}
     
-    computer_name  = "myvm"
-    admin_username = "azureuser"
-    disable_password_authentication = true
+module "appservice" {
+  source           = "./modules/appservice"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "AppService"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
+module "publicip" {
+  source           = "./modules/publicip"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "publicip"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
+# module "vm" {
+#  source           = "./modules/vm"
+#  subnet_id       = "${module.network.subnet_id_test}"
+#  location        = "${var.location}"
+#  resource_group   = "${module.resource_group.resource_group_name}"
+# }
+resource "azurerm_network_interface" "test" {
+  name                = "sjg-udacity-nic"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
 
-    admin_ssh_key {
-        username       = "azureuser"
-        public_key     = file("./az_sjg.pub")
-    }
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = "${module.network.subnet_id_test}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${module.publicip.public_ip_address_id}"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "sjg-udacity-vm"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  size                = "Standard_B1s"
+  admin_username      = "adminsjg"
+  disable_password_authentication = true
+  network_interface_ids = [azurerm_network_interface.test.id]
+  admin_ssh_key {
+    username   = "adminsjg"
+    public_key = "file('./az_sjg.pub')"
+  }
+  os_disk {
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
     
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
-
-resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group (rg) is defined   
-        resource_group = azurerm_resource_group.myterraformgroup.name
-    }
-
-    byte_length = 8
-}
-
-resource "azurerm_storage_account" "mystorageaccount" {
-    name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
-    location                    = "eastus"
-    account_replication_type    = "LRS"
-    account_tier                = "Standard"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
-
-
-resource "azurerm_network_interface" "myterraformnic" {
-    name                        = "myNIC"
-    location                    = "eastus"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
-
-    ip_configuration {
-        name                          = "myNicConfiguration"
-        subnet_id                     = azurerm_subnet.myterraformsubnet.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
-
-# Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "example" {
-    network_interface_id      = azurerm_network_interface.myterraformnic.id
-    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
-}
-resource "azurerm_network_security_group" "myterraformnsg" {
-    name                = "myNetworkSecurityGroup"
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
-
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
-resource "azurerm_subnet" "myterraformsubnet" {
-    name                 = "mySubnet"
-    resource_group_name  = azurerm_resource_group.myterraformgroup.name
-    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
-    address_prefixes       = ["10.0.2.0/24"]
-}
-
-resource "azurerm_public_ip" "myterraformpublicip" {
-    name                         = "myPublicIP"
-    location                     = "eastus"
-    resource_group_name          = azurerm_resource_group.myterraformgroup.name
-    allocation_method            = "Dynamic"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
