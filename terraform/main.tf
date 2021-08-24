189139bd344574d5a04d0d57e118820297ac393c
sgriesmer <sgriesmer@comcast.net>
2021-08-24 12:11:35 -0400

    Update main.tf
    Changed admin_username back to adminsjg.  Caused unexpected issue in build

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,11 +80,11 @@ resource "azurerm_linux_virtual_machine" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   size                = "Standard_B1ms"
-  admin_username      = "azureuser"
+  admin_username      = "adminsjg"
   disable_password_authentication = true
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
-    username   = "azureuser"
+    username   = "adminsjg"
     public_key = file("./az_sjg.pub")
   }
   os_disk {
