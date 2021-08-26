7da7f342b6e469d26d954afecc5b50a2c161902d
sgriesmer <sgriesmer@comcast.net>
2021-08-25 21:59:38 -0400

    Add files via upload
    Changed print statements to standard logging statements.

--- automatedtesting/selenium/select.py
+++ automatedtesting/selenium/select.py
@@ -3,81 +3,85 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.keys import Keys
 
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',level=logging.INFO,datefmt='%Y-%m-%d %H:%M:%S')
+
 
 # Start the browser and login with standard_user
 user = "standard_user"
 password = "secret_sauce"
-print ('Starting the browser...')
+logging.info ('Starting the browser...')
 options = ChromeOptions()
 options.add_argument("--headless") 
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/usr/bin/chromedriver',options=options)
-print ('Browser started successfully. Navigating to the demo page to login.')
+driver = webdriver.Chrome('/opt/WebDriver/bin/chromedriver',options=options)
+logging.info ('Browser started successfully. Navigating to the demo page to login.')
 driver.get('https://www.saucedemo.com/')
 driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
 driver.find_element_by_css_selector("input[id='password']").send_keys(password)
 #driver.find_element_by_css_selector("button[id='login-button']").click()
 driver.find_element_by_css_selector('input[type="submit"]').click()
 
-print ('Current URL:',driver.current_url)
+logging.info (driver.current_url)
 
 backpack = driver.find_element_by_css_selector("button[id='add-to-cart-sauce-labs-backpack']").click()
 
-print ("selected backpack")
+logging.info ("selected backpack")
 
 bikelight = driver.find_element_by_css_selector("button[id='add-to-cart-sauce-labs-bike-light']").click()
 
-print ("selected bikelight")
+logging.info ("selected bikelight")
 
 tshirt = driver.find_element_by_css_selector("button[id='add-to-cart-sauce-labs-bolt-t-shirt']").click()
 
-print ("selected t-shirt")
+logging.info ("selected t-shirt")
 
 fleecejacket = driver.find_element_by_css_selector("button[id='add-to-cart-sauce-labs-fleece-jacket']").click()
 
-print ("selected fleecejacket")
+logging.info ("selected fleecejacket")
 
 onesie = driver.find_element_by_css_selector("button[id='add-to-cart-sauce-labs-onesie']").click()
 
-print ("selected onesie")
+logging.info ("selected onesie")
 
 allthethings = driver.find_element_by_css_selector("button[id='add-to-cart-test.allthethings\(\)-t-shirt-\(red\)']").click()
 
-print ("selected allthethings t-shirt")
+logging.info ("selected allthethings t-shirt")
 
 #driver.save_screenshot('selected.png')
 
 shoppingcart = driver.find_element_by_css_selector("div[id='shopping_cart_container'] > a.shopping_cart_link").click()
 
-print ("selected shopping cart")
+logging.info ("selected shopping cart")
 
 #driver.save_screenshot('shoppingcart.png')
 
-print ("shopping cart URL:", driver.current_url)
+logging.info (driver.current_url)
 
 xbackpack = driver.find_element_by_css_selector("button[id='remove-sauce-labs-backpack']").click()
 
-print ("removed backpack from shopping cart")
+logging.info ("removed backpack from shopping cart")
 
 xbikelight = driver.find_element_by_css_selector("button[id='remove-sauce-labs-bike-light']").click()
 
-print ("removed bikelight from shopping cart")
+logging.info ("removed bikelight from shopping cart")
 
 xtshirt = driver.find_element_by_css_selector("button[id='remove-sauce-labs-bolt-t-shirt']").click()
 
-print ("removed t-shirt from shopping cart")
+logging.info ("removed t-shirt from shopping cart")
 
 xfleecejacket = driver.find_element_by_css_selector("button[id='remove-sauce-labs-fleece-jacket']").click()
 
-print ("removed fleecejacket from shopping cart")
+logging.info ("removed fleecejacket from shopping cart")
 
 xonesie = driver.find_element_by_css_selector("button[id='remove-sauce-labs-onesie']").click()
 
-print ("removed onesie from shopping cart")
+logging.info ("removed onesie from shopping cart")
 
 xallthethings = driver.find_element_by_css_selector("button[id='remove-test.allthethings\(\)-t-shirt-\(red\)']").click()
 
-print ("removed allthethings t-shirt from shopping cart")
+logging.info ("removed allthethings t-shirt from shopping cart")
 
 #driver.save_screenshot('shoppingcart2.png')
